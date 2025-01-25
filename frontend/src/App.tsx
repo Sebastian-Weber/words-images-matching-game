54166c8c14d0d2ca76d53b39b42097b2687bea65
Sebastian Weber <s-weber85@gmx.de>
2025-01-25 18:18:15 +0100

    feat: update description text for clarity and enhance styling with new CSS properties

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import '../src/assets/images/card_1.jpg'
 import SingleCard from './components/SingleCard';
 
-const descriptionText = "Explore unique concepts, emotions, or ideas from different languages that defy direct translation. Each card in the game reveals a cultural gem — either an image or a word. Find the matching pairs to capture those unique concepts, emotions, or ideas that resonate deeply within their origins. Dive into the essence of these words and uncover a new perspective on language and meaning.";
+const descriptionText = "Each card reveals a unique cultural gem — either an image or a word. Discover ideas, emotions and concepts from different languages that defy direct translation and get to know intranslatable words of the world by finding the matching pairs.";
 
 const cardImages = [
   // { "src": "/src/assets/images/card_1.jpg", matched: false, "alt": "Mountain bike Troopers" }, 
@@ -19,7 +19,7 @@ const cardImages = [
   { "src": "/src/assets/images/gluggaveður.png", matched: false, "alt": "gluggaveður", language: "icelandic", description: "Weather that looks appealing from inside but would be unpleasant to be outside in." },
   { "src": "/src/assets/images/hanyauku.png", matched: false, "alt": "hanyauku", language: "kwangali, namibia", description: "The act of walking on tiptoes across warm sand" },
   { "src": "/src/assets/images/komorebi.png", matched: false, "alt": "komorebi", language: "japanese", description: "The dappled light that filters through the leaves of trees." },
-  { "src": "/src/assets/images/mamihlapinatapai.png", matched: false, "alt": "mamihlapinatapai", language: "yaghan, tierra del fuego", description: "The meaningfull look shared by two people who both desire something but are reluctant to initiate." },
+  { "src": "/src/assets/images/mamihlapinatapai.png", matched: false, "alt": "mamihlapinatapai", language: "yaghan", description: "The meaningful look shared by two people who both desire something but are reluctant to initiate." },
   { "src": "/src/assets/images/petrichor.png", matched: false, "alt": "petrichor", language: "greek", description: "A pleasant earthy odor linked to rainfall after warmth and dryness." },
   { "src": "/src/assets/images/tsundoku.png", matched: false, "alt": "tsundoku", language: "japanese", description: "The act of acquiring books and letting them pile up without reading them." },
   { "src": "/src/assets/images/uitwaaien.png", matched: false, "alt": "uitwaaien", language: "dutch", description: "To go out into the wind to refresh your mind or clear your head." },
@@ -97,44 +97,39 @@ useEffect(() => {
     <>
     <div className="wrapper">
       <div className="container">
-        <div className='flex flex-col items-start justify-start h-screen w-auto bg-blue-300'>
+        <div className='flex flex-col items-start justify-start h-screen w-auto'>
 
-          <div className='flex flex-col justify-center items-center w-full bg-purple-400'>
+          <div className='flex flex-col justify-center items-center w-full'>
 
-              <div className='flex flex-col p-2 bg-blue-500'>
-                <div className='w-2/3'>
-                  <h1 className="page-title px-6 py-2">
-                    Lost in Translation
-                  </h1>
-                  <h2 className="page-subtitle">Discover the intranslatable words of the world.</h2>
-                  <br></br>
+              <div className='flex flex-col p-2'>
+                <div className='lg:w-2/3'>
+                  {/* <h1 className="page-title px-6 py-2">
+                  </h1> */}
+                  <h1 className="page-title">Lost in Translation</h1>
                   <p className='page-description'>{descriptionText}</p>
+                  <br></br>
+                  <div className='flex flex-row p-2 w-1/2 justify-between items-center'>
+                    <button onClick={shuffleCards}>New Game</button>
+                    <h4 className='page-counter'>Turns: {turns}</h4>
+                  </div>
                 </div>
-              </div>
-
-              <div className='flex flex-row w-1/3 p-2 justify-between items-center  bg-orange-400'>
-                <button onClick={shuffleCards}>New Game</button>
-                <p>Turns: {turns}</p>
               </div>
 
           </div>
 
           
-          <div className='flex flex-col justify-center items-center  p-2 bg-green-400'>
-
-          <div className="custom-grid">
-
-                {cards.map(card => (
-                  <SingleCard 
-                  key={card.id} 
-                  card={card} 
-                  handleChoice={handleChoice}
-                  flipped={card === choiceOne || card === choiceTwo || card.matched} 
-                  disabled={disabled}
-                  />
-                ))}
-            </div>
-
+          <div className='flex flex-col justify-center items-center'>
+            <div className="custom-grid">
+                  {cards.map(card => (
+                    <SingleCard 
+                    key={card.id} 
+                    card={card} 
+                    handleChoice={handleChoice}
+                    flipped={card === choiceOne || card === choiceTwo || card.matched} 
+                    disabled={disabled}
+                    />
+                  ))}
+              </div>
           </div>
 
         </div>
